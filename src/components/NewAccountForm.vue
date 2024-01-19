64c2aafc2bfe381a406f1370bd0e27c4f94c9035
Abdirahman04 <abdixcom@gmail.com>
2024-01-19 15:03:43 +0300

    added dashboard styling

--- src/components/NewAccountForm.vue
+++ src/components/NewAccountForm.vue
@@ -1,9 +1,9 @@
 <template>
-    <div :class="displayNoneDiv">
+    <div class="rounded-md shadow-md top-44 right-20 p-7 border-2 border-blue-500 bg-white" :class="displayNoneDiv">
         <form>
-            <label for="accountType">Account type</label><br>
-            <input type="text" v-model="createAccountBody.accountType"><br>
-            <button class="btn btn-warning" @click="createAccount">Add</button>
+            <label for="accountType" class="block font-mono">Account type</label>
+            <input class="px-3 py-1 mt-3 mb-2 w-full rounded-md border border-lime-400" type="text" v-model="createAccountBody.accountType">
+            <button class="bg-amber-800 w-full py-1 text-white hover:bg-transparent hover:text-amber-800 border-2 border-amber-800 rounded-xl" @click="createAccount">Add</button>
         </form>
     </div>
 </template>
@@ -22,8 +22,8 @@ export default defineComponent({
         }
     },
     setup(props) {
-        const divClass = reactive<string[]>(["container","position-fixed","top-3","end-3","vw-25","vh-25","bg-light"]);
-        const visible = ref<string>("d-none");
+        const divClass = reactive<string[]>(["fixed"]);
+        const visible = ref<string>("hidden");
 
         const displayNoneDiv = computed(() => {
             if (!props.visible) return [...divClass,visible.value];
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,27 +1,24 @@
 <template>
     <div class="flex flex-col h-screen">
-        <div class="flex justify-between items-center mx-auto bg-amber-950 px-5 py-2">
+        <div class="w-full flex justify-between items-center mx-auto bg-amber-950 px-32 py-2">
             <button @click="backBtn"><i class="fa-solid fa-house"></i></button>
             <h1 class="text-3xl font-semibold text-cyan-500">Dashboard</h1>
-            <button class="bg-red-600 text-white px-4 py-1 hover:bg-transparent hover:text-red-600 border-2 border-red-600" @click="logout">logout</button>
+            <button class="font-bold rounded-md bg-red-600 text-white px-4 py-1 hover:bg-transparent hover:text-red-600 border-2 border-red-600" @click="logout">logout</button>
         </div>
-        <div class="flex flex-col flex-grow bg-slate-300">
-            <h2 class="text-lime-700">Welcome, {{ user.firstName }} {{ user.lastName }}</h2>
-        </div>
-    </div>
-    <div class="container">
-        <button class="btn btn-warning" @click="addAccountBtn">Add +</button>
-        <button class="btn btn-primary" @click="goToProfile">Profile</button>
-        <div class="container">
-            <div class="card" v-for="account in accounts" :key="account.accountId" @click="goToAccount(account)">
-                <div class="card-body">
-                    <div class="card-title">{{ account.accountId }}</div>
-                    <div class="card-text">
-                        <ul>
-                            <li>Account type: {{account.accountType}}</li>
-                            <li>Balance: {{ account.balance }}</li>
-                        </ul>
+        <div class="flex flex-col flex-grow bg-slate-300 p-5">
+            <h2 class="text-lime-900 text-2xl m-3 font-bold">Welcome, {{ user.firstName }} {{ user.lastName }}</h2>
+            <button class="w-24 rounded-lg bg-blue-600 text-white border-2 border-blue-600 hover:bg-transparent hover:text-blue-600 px-3 py-1 font-semibold" @click="goToProfile">Profile</button>
+            <div>
+                <div class="w-1/2 bg-lime-300 mt-3 px-7 py-2 rounded-md shadow-md border border-black" v-for="account in accounts" :key="account.accountId" @click="goToAccount(account)">
+                    <div class="font-serif font-extrabold text-xl">{{ account.accountId }}</div>
+                    <div class="flex justify-between">
+                        <span class="text-slate-700 font-bold text-lg">{{account.accountType}}</span>
+                        <span class="font-semibold font-mono text-red-700">{{ account.balance.toLocaleString('en-US', { style: 'currency', currency: 'USD' }) }}</span>
                     </div>
+                </div>
+                <div @click="addAccountBtn" class="flex justify-between w-1/2 bg-lime-300 mt-3 px-7 py-2 rounded-md shadow-md border border-black">
+                    <span class="font-thin font-serif text-xl">Add account</span>
+                    <span class="text-xl"><i class="fa-solid fa-plus"></i></span>
                 </div>
             </div>
         </div>
