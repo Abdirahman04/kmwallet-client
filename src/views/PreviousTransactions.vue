289c46633851bc8071af85e12c9b5f61a0e08d9d
Abdirahman04 <abdixcom@gmail.com>
2024-01-19 19:47:13 +0300

    added some styling

--- src/views/PreviousTransactions.vue
+++ src/views/PreviousTransactions.vue
@@ -1,22 +1,27 @@
 <template>
-    <div class="container">
-        <h1>Previous Transactions</h1>
-        <back-button path="account" btntitle="Back"></back-button>
-        <div class="card" v-for="transaction in transactionData" :key="transaction.transactionId">
-            <div class="card-body">
-                <div class="card-title">{{ transaction.transactionId }}</div>
-                <div class="card-text">
-                    <p v-if="transaction.transactionType == 'withdraw'">An amount of {{ transaction.balance }} was withdrawn from account {{ transaction.accountId }}</p>
-                    <p v-else-if="transaction.transactionType == 'deposit'">An amount of {{ transaction.balance }} was deposited to account {{ transaction.accountId }}</p>
-                    <p v-else>An amount of {{ transaction.balance }} was sent to account {{ transaction.targetAccount }}</p>
-                </div>
-            </div>
+    <div class="flex flex-col h-screen">
+        <div class="flex justify-between bg-gradient-to-tr from-slate-600 via-slate-200 to-slate-600 px-4 py-2">
+            <button class="text-lime-400 text-3xl" @click="back"><i class="fa-solid fa-arrow-left"></i></button>
+            <h1 class="text-blue-700 font-extrabold font-mono text-3xl">Previous Transactions</h1>
+            <button class="text-red-500 text-3xl" @click="logout"><i class="fa-solid fa-arrow-right-from-bracket"></i></button>
         </div>
-        <div class="card" v-for="transaction in receivedData" :key="transaction.transactionId">
-            <div class="card-body">
-                <div class="card-title">{{ transaction.transactionId }}</div>
-                <div class="card-text">
-                    <p>An amount of {{ transaction.balance }} was received from account {{ transaction.accountId }}</p>
+        <div class="flex flex-grow justify-center items-center">
+            <div class="font-mono w-3/4 px-4 py-2 bg-gradient-to-tr from-slate-600 via-slate-300 to-slate-600 rounded-lg shadow-xl">
+                <div class="px-3 py-1 border border-black rounded-md" v-for="transaction in transactionData" :key="transaction.transactionId">
+                    <div v-if="transaction.transactionType == 'withdraw'">
+                        <p>An amount of {{ transaction.balance }} was withdrawn from account {{ transaction.accountId }}</p>
+                    </div>
+                    <div v-else-if="transaction.transactionType == 'deposit'">
+                        <p>An amount of {{ transaction.balance }} was deposited to account {{ transaction.accountId }}</p>
+                    </div>
+                    <div v-else>
+                        <p>An amount of {{ transaction.balance }} was sent to account {{ transaction.targetAccount }}</p>
+                    </div>
+                </div>
+                <div class="px-3 py-1 border border-black rounded-md" v-for="transaction in receivedData" :key="transaction.transactionId">
+                    <div>
+                        <p>An amount of {{ transaction.balance }} was received from account {{ transaction.accountId }}</p>
+                    </div>
                 </div>
             </div>
         </div>
@@ -26,12 +31,15 @@
 <script lang="ts">
 import BackButton from '@/components/BackButton.vue'
 import { defineComponent, reactive, onMounted } from 'vue'
+import { useRouter } from 'vue-router'
 import { getData } from '@/assets/apiService'
 import { Transaction } from '@/types/transactions'
 
 export default defineComponent({
   components: { BackButton },
     setup() {
+        const router = useRouter();
+
         const transactionData = reactive<Transaction[]>([]);
 
         const receivedData = reactive<Transaction[]>([]);
@@ -61,9 +69,20 @@ export default defineComponent({
             getReceivedData();
         });
 
+        function back() {
+            router.push({ name: "account" });
+        }
+
+        function logout() {
+            localStorage.clear();
+            router.push({ name: "home" });
+        }
+
         return{
             transactionData,
-            receivedData
+            receivedData,
+            back,
+            logout
         }
         
     },
